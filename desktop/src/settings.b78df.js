022f4d8b26e0fa7e5a0a6f1ecd0cee04c084543c
Next <1406099478@qq.com>
2018-03-06 19:55:18 +0800

    添加拼图游戏

--- desktop/src/settings.b78df.js
+++ desktop/src/settings.b78df.js
@@ -14,8 +14,24 @@ window._CCSettings = {
                 "res/textures/bg.jpg",
                 0
             ],
+            "1bRNMF3NNFdKkqXCkIAxrR": [
+                "res/textures/circle.png",
+                0
+            ],
+            "aaZnPsv6ZFVJNLY/270+U3": [
+                "res/textures/jieyi.png",
+                0
+            ],
+            "e7bVLkieZANatUaOxb3GaB": [
+                "res/textures/square.png",
+                0
+            ],
             "6fdqXkM/dFx4Z8owYEthUI": [
                 "res/textures/streak.png",
+                0
+            ],
+            "1eyoE3dRZLcp4DtSAka4q4": [
+                "res/textures/streak_05_001.png",
                 0
             ]
         },
@@ -34,6 +50,22 @@ window._CCSettings = {
             ],
             "d8HsitJHxOYqo801xBk8ev": [
                 "image/default_panel.png",
+                0
+            ],
+            "cf73jxyN9Jt47QTJU6ziYh": [
+                "image/default_progressbar.png",
+                0
+            ],
+            "99FwsL0hBG8bITfZ4/IwmK": [
+                "image/default_progressbar_bg.png",
+                0
+            ],
+            "56fc2Ai/RFNYpaMT8crweK": [
+                "image/default_radio_button_off.png",
+                0
+            ],
+            "9dYAAftfRHJqYpJlnj3tC4": [
+                "image/default_radio_button_on.png",
                 0
             ],
             "d608qFRoFHwbXd0Dap056i": [
@@ -57,7 +89,7 @@ window._CCSettings = {
     scenes: [
         {
             url: "db://assets/res/scenes/loding.fire",
-            uuid: 5
+            uuid: 7
         },
         {
             url: "db://assets/res/scenes/gobang.fire",
@@ -65,15 +97,23 @@ window._CCSettings = {
         },
         {
             url: "db://assets/res/scenes/hall.fire",
-            uuid: 4
+            uuid: 6
+        },
+        {
+            url: "db://assets/res/scenes/jump.fire",
+            uuid: 5
         },
         {
             url: "db://assets/res/scenes/2048.fire",
             uuid: 1
         },
         {
+            url: "db://assets/res/scenes/puzzle.fire",
+            uuid: 4
+        },
+        {
             url: "db://assets/res/scenes/reversi.fire",
-            uuid: 6
+            uuid: 8
         }
     ],
     packedAssets: {
@@ -89,50 +129,81 @@ window._CCSettings = {
             3,
             "e8WbTgyiRHC51Pr1bvX6Oe"
         ],
+        "0706b59e8": [
+            "1aMvx28L1PZpgPVpKcDKCz",
+            "29DBvjr/JFIKAE6p4+XRP4",
+            2,
+            "d0uPTKqIBICYd4eG64Ey97",
+            "e7q6FL+VZEgLJUjVeDLic/",
+            4
+        ],
+        "090a99623": [
+            "0dUfa9dYFH+Z7vb7F+QaxK",
+            "45anNQJuNMRpWUI0r5vCjF",
+            "675ovJ2tVK2aLYfgPUWOMv",
+            "8855/VlrRKd6H0MSRnFxAU",
+            2,
+            "9fQHYCOmVNh4IdI6jnPD6r",
+            5
+        ],
         "0b1bf8e52": [
             "29FYIk+N1GYaeWH/q1NxQO",
             "5cO7kybDxGj4ipyMYdRYZB",
             "5f5dyqtRNNxaFmVzYns6FZ",
-            4,
+            6,
             2,
             "9bvaMerUlDyary99mJa6xp",
             "e97GVMl6JHh5Ml5qEDdSGa",
             "f0BIwQ8D5Ml7nTNQbh1YlS"
         ],
         "0b7146e97": [
-            5,
+            7,
             "4fhQ8WQRxAr4BjnTF7Byv1",
             2,
             3
         ],
         "0c49ae998": [
-            6,
+            8,
             2,
             3
         ]
     },
-    orientation: "portrait",
+    orientation: "",
     uuids: [
         "04pOBcxcNN4ItwvVI9w3ae",
         "25mUoCgM1DX6AX9srx0UWQ",
         "94nZeiRMRHOI4JOOrCtcLD",
         "a2MjXRFdtLlYQ5ouAFv/+R",
+        "f0VE8ZW5FBJ7wrJMF5vn3V",
+        "a2iLu4Y9xJX6NaE5Lmkt9T",
         "8acfqbljRHXK2shXdwXQRJ",
         "0dnG4Oh6tGRbZ3rrL5WUw/",
         "06Xx+0McJFHpmNoJBf16Cv"
     ],
     "md5AssetsMap": {
-        "03/03f7ca26f.json": "f6b17",
-        "06/0629afa22.json": "cbf42",
-        "0b/0b1bf8e52.json": "10f0e",
-        "0b/0b7146e97.json": "5b010",
-        "0c/0c49ae998.json": "7dd27",
+        "03/03f7ca26f.json": "52993",
+        "06/0629afa22.json": "5a785",
+        "07/0706b59e8.json": "ccac0",
+        "09/090a99623.json": "dd86a",
+        "0b/0b1bf8e52.json": "957a3",
+        "0b/0b7146e97.json": "beb2f",
+        "0c/0c49ae998.json": "9eaa0",
+        "c6/c6e87de6-c9df-4ae7-b9db-3812d622863f.json": "14a51",
+        "c6/c6e87de6-c9df-4ae7-b9db-3812d622863f/zhankukuaile.ttf": "0acea",
         "assets/res/textures/bg.jpg": "e7b3b",
+        "assets/res/textures/circle.png": "f2b24",
+        "assets/res/textures/jieyi.png": "4192e",
+        "assets/res/textures/square.png": "89d13",
+        "assets/res/textures/streak_05_001.png": "64d59",
         "assets/res/textures/streak.png": "34961",
         "internal/image/default_btn_disabled.png": "286c6",
         "internal/image/default_btn_normal.png": "1ecb7",
         "internal/image/default_btn_pressed.png": "bedf4",
         "internal/image/default_panel.png": "cdbc9",
+        "internal/image/default_progressbar_bg.png": "9cdfa",
+        "internal/image/default_progressbar.png": "69ff9",
+        "internal/image/default_radio_button_off.png": "acdf0",
+        "internal/image/default_radio_button_on.png": "94752",
         "internal/image/default_scrollbar_vertical_bg.png": "4bb41",
         "internal/image/default_scrollbar_vertical.png": "71821",
         "internal/image/default_sprite_splash.png": "cea68"
--- mobile/main.d9bf9.js
+++ mobile/main.d9bf9.js
@@ -137,7 +137,7 @@
 
         // jsList
         var jsList = settings.jsList;
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.e5bce.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.c3cd1.js';
         if (jsList) {
             jsList = jsList.map(function (x) { return 'src/' + x; });
             jsList.push(bundledScript);
@@ -170,7 +170,7 @@
     }
 
     if (window.jsb) {
-        require('src/settings.5c9f3.js');
+        require('src/settings.9e9c5.js');
         require('src/jsb_polyfill.js');
         boot();
     }
