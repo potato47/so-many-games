ded7a0f1b5151c8c5e712f27224af7de9e59ac29
next <1406099478@qq.com>
2018-04-03 02:11:28 +0800

    扫雷闯关版

---
+++ mobile/src/settings.47ae9.js
@@ -0,0 +1,483 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "bewjdKHd5Hiq8BSMyHunao": [
+                "res/textures/GrayBlockFX.png",
+                0
+            ],
+            "bcpDX4eD1EsbukLcualSca": [
+                "res/textures/bg.jpg",
+                0
+            ],
+            "a1xeADeA1P0Y8QV3m43+iA": [
+                "res/textures/button/shadedDark/shadedDark24.png",
+                0
+            ],
+            "85m/APVvFCUYZBT/O0WQJW": [
+                "res/textures/button/shadedDark/shadedDark25.png",
+                0
+            ],
+            "21pMxwrHVInIA/5ndfG0BJ": [
+                "res/textures/button/shadedDark/shadedDark27.png",
+                0
+            ],
+            "4d/f9z60lKFKionlpFZQdM": [
+                "res/textures/button/shadedDark/shadedDark48.png",
+                0
+            ],
+            "caTQoOcTZC5J/TIEamyw9E": [
+                "res/textures/button/shadedDark/shadedDark49.png",
+                0
+            ],
+            "1bRNMF3NNFdKkqXCkIAxrR": [
+                "res/textures/circle.png",
+                0
+            ],
+            "aaZnPsv6ZFVJNLY/270+U3": [
+                "res/textures/jieyi.png",
+                0
+            ],
+            "32qbyhjoBBlb7rVaGXseCP": [
+                "res/textures/mine/blank.png",
+                0
+            ],
+            "e83cNjcNVMvrcfs19pe6i4": [
+                "res/textures/mine/bombdeath.png",
+                0
+            ],
+            "eexThXHnZAsq4moriTGe+q": [
+                "res/textures/mine/bombflagged.png",
+                0
+            ],
+            "57RFgW/ZdFbb3jnu8DHU3W": [
+                "res/textures/mine/bombrevealed.png",
+                0
+            ],
+            "69vaAHeGdCfoYc6MhliOvh": [
+                "res/textures/mine/open0.png",
+                0
+            ],
+            "35r5cMHvpPj69pdSCgq9tl": [
+                "res/textures/mine/open1.png",
+                0
+            ],
+            "74LgSpMfpDWZAcr9vZAKj/": [
+                "res/textures/mine/open2.png",
+                0
+            ],
+            "d2hxDf0sxAeLt0QqBcWHoB": [
+                "res/textures/mine/open3.png",
+                0
+            ],
+            "35plTozvZNh4E2QIu5tQmD": [
+                "res/textures/mine/open4.png",
+                0
+            ],
+            "d9qhLNwjpKCo9+b0h+5alO": [
+                "res/textures/mine/open5.png",
+                0
+            ],
+            "36XIUDQStBJrDIG1lHSrx0": [
+                "res/textures/mine/open6.png",
+                0
+            ],
+            "d3xmk/nBBNkJoBEhD/n3Iu": [
+                "res/textures/mine/open7.png",
+                0
+            ],
+            "69MFjq2kRB1Jli5xsDoC/X": [
+                "res/textures/mine/open8.png",
+                0
+            ],
+            "4au9ZlcVNMmqqXyUdqav+w": [
+                "res/textures/mine/singleColor.png",
+                0
+            ],
+            "9233m2jmdANoSAtWVUVpyC": [
+                "res/textures/mine/time0.png",
+                0
+            ],
+            "c5NPYOGNNDerAssfdBwdak": [
+                "res/textures/mine/time1.png",
+                0
+            ],
+            "245tjufmZDXbElEF7cMdzE": [
+                "res/textures/mine/time2.png",
+                0
+            ],
+            "ebF9qrFdxOvKIqYV31rh5s": [
+                "res/textures/mine/time3.png",
+                0
+            ],
+            "50ESOhCH1FyoQ8FkF99l9p": [
+                "res/textures/mine/time4.png",
+                0
+            ],
+            "2aCPL+fttF756C3+I+Hfqv": [
+                "res/textures/mine/time5.png",
+                0
+            ],
+            "1b1u/hwb9A2ar5oDCZCMyx": [
+                "res/textures/mine/time6.png",
+                0
+            ],
+            "b17wR+g7hH4qmPNnYqGB4j": [
+                "res/textures/mine/time7.png",
+                0
+            ],
+            "99b8HmoOpP+KngDVqTx8Lw": [
+                "res/textures/mine/time8.png",
+                0
+            ],
+            "8fDL/5MoVDVIqc5ryuGpqK": [
+                "res/textures/mine/time9.png",
+                0
+            ],
+            "c4DciyLP5NdKyhTX+pS1hf": [
+                "res/textures/number/Eight.png",
+                0
+            ],
+            "93Hv1HmZNArquzIQBdz7hh": [
+                "res/textures/number/Five.png",
+                0
+            ],
+            "af7+vpW/ZJBpHgsAu/XuQG": [
+                "res/textures/number/Four.png",
+                0
+            ],
+            "ccNAlhpPlIYJ25chwxvfJE": [
+                "res/textures/number/Nine.png",
+                0
+            ],
+            "892k7TjSxA/qBuH75uRITZ": [
+                "res/textures/number/One.png",
+                0
+            ],
+            "b5HaF28CBBKpviLfPRao9L": [
+                "res/textures/number/Seven.png",
+                0
+            ],
+            "6fIlgD91ZMzYa6l+9LMQzq": [
+                "res/textures/number/Six.png",
+                0
+            ],
+            "43WpX5y1RIFI7fg6ej7S8i": [
+                "res/textures/number/Three.png",
+                0
+            ],
+            "4caT2i6lZNMoyZE9LUEXzG": [
+                "res/textures/number/Two.png",
+                0
+            ],
+            "e7bVLkieZANatUaOxb3GaB": [
+                "res/textures/square.png",
+                0
+            ],
+            "6fdqXkM/dFx4Z8owYEthUI": [
+                "res/textures/streak.png",
+                0
+            ],
+            "1eyoE3dRZLcp4DtSAka4q4": [
+                "res/textures/streak_05_001.png",
+                0
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                0
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                0
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                0
+            ],
+            "d8HsitJHxOYqo801xBk8ev": [
+                "image/default_panel.png",
+                0
+            ],
+            "cf73jxyN9Jt47QTJU6ziYh": [
+                "image/default_progressbar.png",
+                0
+            ],
+            "99FwsL0hBG8bITfZ4/IwmK": [
+                "image/default_progressbar_bg.png",
+                0
+            ],
+            "56fc2Ai/RFNYpaMT8crweK": [
+                "image/default_radio_button_off.png",
+                0
+            ],
+            "9dYAAftfRHJqYpJlnj3tC4": [
+                "image/default_radio_button_on.png",
+                0
+            ],
+            "d608qFRoFHwbXd0Dap056i": [
+                "image/default_scrollbar_vertical.png",
+                0
+            ],
+            "61cyPdEfRN047sDK9rO0W5": [
+                "image/default_scrollbar_vertical_bg.png",
+                0
+            ],
+            "02delMVqdBD70a/HSD99FK": [
+                "image/default_sprite_splash.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/res/scenes/loding.fire",
+    scenes: [
+        {
+            url: "db://assets/res/scenes/loding.fire",
+            uuid: 10
+        },
+        {
+            url: "db://assets/res/scenes/get47.fire",
+            uuid: 7
+        },
+        {
+            url: "db://assets/res/scenes/gobang.fire",
+            uuid: 0
+        },
+        {
+            url: "db://assets/res/scenes/hall.fire",
+            uuid: 9
+        },
+        {
+            url: "db://assets/res/scenes/jump.fire",
+            uuid: 8
+        },
+        {
+            url: "db://assets/res/scenes/2048.fire",
+            uuid: 4
+        },
+        {
+            url: "db://assets/res/scenes/mine.fire",
+            uuid: 1
+        },
+        {
+            url: "db://assets/res/scenes/puzzle.fire",
+            uuid: 5
+        },
+        {
+            url: "db://assets/res/scenes/reversi.fire",
+            uuid: 11
+        },
+        {
+            url: "db://assets/res/scenes/tetris.fire",
+            uuid: 6
+        }
+    ],
+    packedAssets: {
+        "03f7ca26f": [
+            0,
+            2,
+            3
+        ],
+        "051cfbbc8": [
+            "05/WszEMpLH4t+I2U9xNar",
+            "4dFiSlDUlPkrXgK2DhraeQ",
+            "5dbGvasVBD/6H0An9q/Vhs",
+            "60fTeVp2RCAZYEFRq3x0K0",
+            "64TVbHgT5Gla8VkiPxZnLH",
+            "67510fFZhExIjr+1hHFGRr",
+            "75j8AoiJ1DzqCgnU9oaWd/",
+            "7dH4CpRZNAj7ICJ4MOcQjw",
+            "81QWCouddOX49CMHIrplan",
+            "84c3+31f5LVI3WI2izEUbh",
+            "86iRsMBx5Df4i6sM55EmOo",
+            "8bIGev9b5GDLSY2CiLetCn",
+            1,
+            2,
+            "9dQHfaM5hEcq6PXbklJFbe",
+            3,
+            "a489UXXiRJ8r/LM5skjQHp",
+            "a9ToWuhmJFT4APvvN/ZSRV",
+            "acnFpxA61Oor3s1bg8WwLE",
+            "afQwqtQeZHsp0zD0m4nv7+",
+            "c2vpHbHv9Lr5rhuKg0I8+Z",
+            "d4sCqoGUpJ1IYzEsuebGDE",
+            "dfgZEp701CnZrI1gDkz9rK",
+            "f1qvh3sRVJHbUcwBNzS7nG",
+            "f2oDKetddJ2qDp8Uf938qR",
+            "f2xcSPAORKkqSjtqhxYICI",
+            "f5hzmiS3BCKY3mNvmmsOFo",
+            "f6Cr4xNANHSqdI6IlB7rJR"
+        ],
+        "0629afa22": [
+            4,
+            "30RpVnDRRGpb5Qf/sQlnIv",
+            2,
+            3,
+            "e8WbTgyiRHC51Pr1bvX6Oe"
+        ],
+        "0706b59e8": [
+            "1aMvx28L1PZpgPVpKcDKCz",
+            "29DBvjr/JFIKAE6p4+XRP4",
+            2,
+            "d0uPTKqIBICYd4eG64Ey97",
+            "e7q6FL+VZEgLJUjVeDLic/",
+            5
+        ],
+        "084ac5488": [
+            "1dii5HFjJLMphUCSczDslP",
+            "22pLrCFdRJD7STFkdvAxLl",
+            "85xA7zw5pABbhucFQsFYdX",
+            2,
+            "9dK7S3aihMM5BlXG+RlQ+Z",
+            3,
+            6,
+            "c0NUPjRGVBDL/pSxBUAPOx",
+            "d6TNQ5UKNBOKRqan6Zak0J",
+            "d7IeqKxE9NcZ9KUwPJ0Tl6"
+        ],
+        "0889b0b0b": [
+            "025lE0eT1JWo9GrbRH2Cm7",
+            7,
+            "13A9FWqL9MDLq29CqNrROv",
+            "1dqxaWi8ZPD4RMMfIw6HYk",
+            "44Yfc/gnRI0JWHBAlRXtX6",
+            "4ewStZS1tA752H89ioDgjl",
+            "6f5Nu99V1FXbvLHexiXUDc",
+            "76eq7GTYhAMK5529Q4oNkj",
+            "7erlmxw/pDnJhnk9h/MUWY",
+            "8dp6eB/kFM0qw36iHjqQYk",
+            2,
+            3,
+            "adAuLo2M1EyIKBB0vQ5KA5",
+            "beh7eFwNFPma0dPbnH7PKL",
+            "c7W/B+K4VBtbZ6FAlCp9pc"
+        ],
+        "090a99623": [
+            "0dUfa9dYFH+Z7vb7F+QaxK",
+            "45anNQJuNMRpWUI0r5vCjF",
+            "675ovJ2tVK2aLYfgPUWOMv",
+            "8855/VlrRKd6H0MSRnFxAU",
+            2,
+            "9fQHYCOmVNh4IdI6jnPD6r",
+            8
+        ],
+        "0b1bf8e52": [
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "5cO7kybDxGj4ipyMYdRYZB",
+            "5f5dyqtRNNxaFmVzYns6FZ",
+            9,
+            2,
+            "9bvaMerUlDyary99mJa6xp",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ],
+        "0b7146e97": [
+            10,
+            "4fhQ8WQRxAr4BjnTF7Byv1",
+            2,
+            3
+        ],
+        "0c49ae998": [
+            11,
+            2,
+            3
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "04pOBcxcNN4ItwvVI9w3ae",
+        "92XhYUw15Ku7wgk50XWDUt",
+        "94nZeiRMRHOI4JOOrCtcLD",
+        "a2MjXRFdtLlYQ5ouAFv/+R",
+        "25mUoCgM1DX6AX9srx0UWQ",
+        "f0VE8ZW5FBJ7wrJMF5vn3V",
+        "adMjFLfQhMEpyAhFFeGien",
+        "08g05AO35JTrQWW4NnnwHr",
+        "a2iLu4Y9xJX6NaE5Lmkt9T",
+        "8acfqbljRHXK2shXdwXQRJ",
+        "0dnG4Oh6tGRbZ3rrL5WUw/",
+        "06Xx+0McJFHpmNoJBf16Cv"
+    ],
+    "md5AssetsMap": {
+        "03/03f7ca26f.json": "52993",
+        "05/051cfbbc8.json": "d2725",
+        "06/0629afa22.json": "5a785",
+        "07/0706b59e8.json": "ccac0",
+        "08/084ac5488.json": "67441",
+        "08/0889b0b0b.json": "ec282",
+        "09/090a99623.json": "dd86a",
+        "0b/0b1bf8e52.json": "cfa21",
+        "0b/0b7146e97.json": "beb2f",
+        "0c/0c49ae998.json": "9eaa0",
+        "c6/c6e87de6-c9df-4ae7-b9db-3812d622863f.json": "14a51",
+        "c6/c6e87de6-c9df-4ae7-b9db-3812d622863f/zhankukuaile.ttf": "0acea",
+        "assets/res/textures/bg.jpg": "e7b3b",
+        "assets/res/textures/button/shadedDark/shadedDark24.png": "ea69f",
+        "assets/res/textures/button/shadedDark/shadedDark25.png": "7ee00",
+        "assets/res/textures/button/shadedDark/shadedDark27.png": "61e65",
+        "assets/res/textures/button/shadedDark/shadedDark48.png": "da1cb",
+        "assets/res/textures/button/shadedDark/shadedDark49.png": "9e183",
+        "assets/res/textures/circle.png": "f2b24",
+        "assets/res/textures/GrayBlockFX.png": "e0553",
+        "assets/res/textures/jieyi.png": "4192e",
+        "assets/res/textures/mine/blank.png": "1324d",
+        "assets/res/textures/mine/bombdeath.png": "7caf2",
+        "assets/res/textures/mine/bombflagged.png": "2f389",
+        "assets/res/textures/mine/bombrevealed.png": "59031",
+        "assets/res/textures/mine/open0.png": "748bf",
+        "assets/res/textures/mine/open1.png": "41196",
+        "assets/res/textures/mine/open2.png": "fcd14",
+        "assets/res/textures/mine/open3.png": "c6258",
+        "assets/res/textures/mine/open4.png": "7a417",
+        "assets/res/textures/mine/open5.png": "c654c",
+        "assets/res/textures/mine/open6.png": "320b2",
+        "assets/res/textures/mine/open7.png": "5724e",
+        "assets/res/textures/mine/open8.png": "2c2ee",
+        "assets/res/textures/mine/singleColor.png": "e5136",
+        "assets/res/textures/mine/time0.png": "d8080",
+        "assets/res/textures/mine/time1.png": "7e3d4",
+        "assets/res/textures/mine/time2.png": "30e0b",
+        "assets/res/textures/mine/time3.png": "f3b6a",
+        "assets/res/textures/mine/time4.png": "208cf",
+        "assets/res/textures/mine/time5.png": "35391",
+        "assets/res/textures/mine/time6.png": "01e38",
+        "assets/res/textures/mine/time7.png": "2b47b",
+        "assets/res/textures/mine/time8.png": "55e24",
+        "assets/res/textures/mine/time9.png": "89cb9",
+        "assets/res/textures/number/Eight.png": "3be06",
+        "assets/res/textures/number/Five.png": "49e76",
+        "assets/res/textures/number/Four.png": "16358",
+        "assets/res/textures/number/Nine.png": "917f0",
+        "assets/res/textures/number/One.png": "1473b",
+        "assets/res/textures/number/Seven.png": "e3a13",
+        "assets/res/textures/number/Six.png": "dabf6",
+        "assets/res/textures/number/Three.png": "da501",
+        "assets/res/textures/number/Two.png": "377f8",
+        "assets/res/textures/square.png": "89d13",
+        "assets/res/textures/streak_05_001.png": "64d59",
+        "assets/res/textures/streak.png": "34961",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_normal.png": "1ecb7",
+        "internal/image/default_btn_pressed.png": "bedf4",
+        "internal/image/default_panel.png": "cdbc9",
+        "internal/image/default_progressbar_bg.png": "9cdfa",
+        "internal/image/default_progressbar.png": "69ff9",
+        "internal/image/default_radio_button_off.png": "acdf0",
+        "internal/image/default_radio_button_on.png": "94752",
+        "internal/image/default_scrollbar_vertical_bg.png": "4bb41",
+        "internal/image/default_scrollbar_vertical.png": "71821",
+        "internal/image/default_sprite_splash.png": "cea68"
+    }
+};
